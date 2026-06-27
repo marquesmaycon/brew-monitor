2a388d78d5d4522675f8bc42014a1e76c259d912
marquesmaycon <mayconmarquesh@gmail.com>
2026-06-26 23:24:02 -0300

    fix namespace

--- api/Data/AppDbContext.cs
+++ api/Data/AppDbContext.cs
@@ -2,10 +2,6 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ArBrain.Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-  public AppDbContext(DbContextOptions<AppDbContext> options)
-      : base(options)
-  {
-  }
 }

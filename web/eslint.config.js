b7cf0bf41f47a130ac62bf6a49efab0de4a2c728
marquesmaycon <mayconmarquesh@gmail.com>
2026-06-28 18:51:39 -0300

    web: init

---
+++ web/eslint.config.js
@@ -0,0 +1,29 @@
+//  @ts-check
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { tanstackConfig } from '@tanstack/eslint-config'
+
+export default [
+  ...tanstackConfig,
+  {
+    rules: {
+      'import/no-cycle': 'off',
+      'import/order': 'off',
+      'sort-imports': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/require-await': 'off',
+      'pnpm/json-enforce-catalog': 'off',
+    },
+  },
+  {
+    ignores: ['eslint.config.js', 'prettier.config.js'],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+]

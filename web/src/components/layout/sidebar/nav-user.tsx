825bfc1d475aee7d429a30829050f8069df97ff9
marquesmaycon <mayconmarquesh@gmail.com>
2026-07-06 11:07:19 -0300

    web: fix user nav

--- web/src/components/layout/sidebar/nav-user.tsx
+++ web/src/components/layout/sidebar/nav-user.tsx
@@ -64,7 +64,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">BM</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">{user.name}</span>
